30e7a1656d7e9200a5b7c46ca732a79c11100aa7
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-07-26 14:40:22 -0700

    Add files via upload
    finding-character-at-location-js

---
+++ finding-character-at-location-js/index.js
@@ -0,0 +1,30 @@
+// CHAPTER 24: FINDING A CHARACTER AT A LOCATION
+
+// METHOD We'LL DISCUSS
+// charAt
+
+// const institute = "saylani mass it training";
+// console.log(institute.charAt(14));
+
+// const fruit= "apple";
+// console.log(fruit.charAt(1));
+
+// If you want to access space
+// const greetings = "hi how are you?"
+// console.log(greetings.charAt(2));
+
+// Email validation using charAt
+// const userEmail =prompt("please enter your email address");
+// let isvalid = false;
+
+// for(let charindex = 0; charindex < userEmail.length; charindex++ ){
+//    if(userEmail.charAt(charindex) === "@"){
+//       isvalid = true;
+//       break;
+//    }
+// }
+// if(isvalid){
+//     console.log("valid email address");
+// }else{
+//     console.log("invaild email address");
+// }
